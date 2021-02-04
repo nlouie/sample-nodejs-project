811efe2700d1fc9be18e7000995357c6c4a29f29
nlouie <nick@nlouie.com>
2021-02-04 17:09:33 -0500

    adds crypto example

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+function getName(){
+  return "Nick"
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', firstName: getName() });
+});
+
+/* GET anout page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
 });
 
 module.exports = router;
